7430054340ed27293ac501d4b8894811b1809e93
tevin <gatheruteving@gmail.com>
2023-08-26 11:25:22 +0300

    implement patient add

--- medbook-dev-frontend/src/app/app.module.ts
+++ medbook-dev-frontend/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientDetailsComponent } from './patient-details/patient-details.component';
+import { FormsModule } from '@angular/forms';
+import { PatientListComponent } from './patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientDetailsComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
